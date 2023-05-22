45bab53afa3d9c5e78f46a9aac484acd0963caad
Adolf <owner@Users-MacBook-Air.local>
2023-05-21 17:12:40 -0700

    add pages

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import sanity from 'astro-sanity';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+      projectId: 'sgg58wrl',
+      dataset: 'production',
+      apiVersion: '2023-05-21',
+      useCdn: false,
+    }),
+  ],
+});
